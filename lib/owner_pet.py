70913985cb96fecf2da709e9adb4f0a36864e816
Eric Nelson <ernelson12o0@gmail.com>
2023-09-19 16:54:33 -0400

    Lab Complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,33 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception(f'Invalid pet type: {pet_type}')
+        else:
+            self.name = name
+            self.pet_type = pet_type
+            self.owner = owner
+            Pet.all.append(self)
+
 
 class Owner:
-    pass
+    
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if pet in Pet.all:
+            pet.owner = self
+        else:
+            raise Exception(f'Invalid pet: {pet}')
+        
+    def get_sorted_pets(self):
+        owner_pets = self.pets()
+        sorted_pets = sorted(owner_pets, key= lambda pet: pet.name)
+        return sorted_pets
